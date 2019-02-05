7d71de55e9a8f2af6ebb74056a921c05fb4f4764
garoot <a.majeed.garoot@hotmail.com>
2019-02-05 00:21:54 -0800

    - edited channel registration page
    next:
    -finish up the registration page
    -check the urls
    -add buttons to navbar in base.html
    
    ...next.next:
    1-add profile decoration to channels
    2-or research adding media platform (CSS & JavaScript)
    3-Lesson reviews
    4-courses views

--- EduControl/urls.py
+++ EduControl/urls.py
@@ -3,4 +3,5 @@ from EduControl import views
 
 urlpatterns = [
     url(r'^$', views.broadcast_station, name='broadcast_station'),
+    url(r'^channelRegister', views.register, name='channel_registration')
 ]
