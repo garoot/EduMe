50e79cbc4ead15e8e39271bbf75a1512b7c0018f
garoot <a.majeed.garoot@hotmail.com>
2019-02-04 22:49:01 -0800

    -added the registration function in views.py ..now adding registration page to templates

--- EduControl/views.py
+++ EduControl/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from EduControl.forms import UserForm, UserProfileInfoForm
 
 # Create your views here.
 def broadcast_station(request):
@@ -6,3 +7,19 @@ def broadcast_station(request):
 
 def index(request):
     return render(request, 'EduMe/index.html')
+
+def register(request):
+    registered = False
+    user_form = UserForm()
+    if request.method == "POST":
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+            #or you can return a "Successful Registration" page
+
+        else:
+            print("There was an error with one one of the fields")
+
+    return render(request, 'EduControl/register.html', {'form':form})
